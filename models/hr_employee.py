44f54a0821d2070107c99b7fadd2f41371df9e3a
drzaphod85 <lasse@familjenlarsson.eu>
2025-04-11 12:38:33 +0200

    Corrected all files for bugs. Still some there.

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -132,8 +132,8 @@ class ResMunicipality(models.Model):
     code = fields.Char(required=True)
     region_id = fields.Many2one('res.region', string='Region', required=True)
     tax_table_number = fields.Char(required=True)
-    total_tax_rate = fields.Float(string='Total Tax Rate', required=True)
-    church_tax_rate = fields.Float(string='Church Tax Rate', required=True)
+    total_tax_rate = fields.Float(string='Total Tax Rate', required=False)
+    church_tax_rate = fields.Float(string='Church Tax Rate', required=False)
 
     _sql_constraints = [
         ('code_unique', 'unique(code)', 'Municipality code must be unique.')
